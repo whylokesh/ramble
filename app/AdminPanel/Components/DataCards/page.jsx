8b23a6ba02389463ff5851be2589110adcef45b3
Lokesh <jhalokesh187@gmail.com>
2023-11-30 15:20:02 +0530

    dashboard changes

--- app/AdminPanel/Components/DataCards/page.jsx
+++ app/AdminPanel/Components/DataCards/page.jsx
@@ -1,72 +1,136 @@
 "use clients";
 import React from "react";
 import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import { CircularProgress, CardFooter, Chip } from "@nextui-org/react";
 
 export default function DataCard() {
   return (
     <>
-      <h1 className="font-bold text-4xl px-8 mt-6 flex justify-center items-center">Dashboard</h1>
-    <div className="flex flex-wrap lg:justify-between md:justify-between justify-center  items-center lg:p-12 md:p-10 p-4 ">
-    <Card className="py-4 mt-4 ">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start lg:m-0 md-m-0 m-auto">
-          <div className="flex justify-between items-center w-full">
-            <div>
-            <h4 className="font-bold text-large mb-2 w-[12rem]">Orders</h4>
-              <p className="text-md uppercase mb-2 bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger bg-clip-text text-transparent font-bold">28</p>
-              {/* <small className="text-default-500">12  Clients Approached</small> */}
-              
-            </div>
-            <CardBody className="overflow-visible py-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"  className=" text-black p-1 h-9 bg-gradient-to-r from-[#fbdfb3] to-[#5bffde] to-danger border-solid rounded-md">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-</svg>
-
-
-            </CardBody>
-                  
+      {/* <h1 className="font-bold text-4xl px-8 mt-6 flex justify-center items-center">Dashboard</h1> */}
+      <div className="flex gap-8 lg:p-12 md:p-10 p-4">
+        <div className="w-4/5 flex-row">
+          <div className="lg:w-full md:w-full w-full flex gap-6 bg-black lg:mt-0 md:mt-5 mt-5" >
+            <Card className=" lg:mt-0 md:mt-0 mt-1 w-1/2 h-[17rem] border-none  bg-gradient-to-br from-[#9C27B0] to-[#E500FF]">
+              <CardBody className="justify-center items-center pb-0">
+                <CircularProgress
+                  classNames={{
+                    svg: "w-36 h-36 drop-shadow-md",
+                    indicator: "stroke-white",
+                    track: "stroke-white/10",
+                    value: "text-3xl font-semibold text-white",
+                  }}
+                  value={70}
+                  strokeWidth={4}
+                  showValueLabel={true}
+                />
+              </CardBody>
+              <CardFooter className="justify-center items-center pt-0">
+                <Chip
+                  classNames={{
+                    base: "border-1 border-white/30",
+                    content: "text-white/90 text-small font-semibold",
+                  }}
+                  variant="bordered"
+                >
+                  2800 Clients Approached
+                </Chip>
+              </CardFooter>
+            </Card>
+            <Card className=" lg:mt-0 md:mt-0 w-1/2 h-[17rem] border-none  bg-gradient-to-br from-[#ffcc00] to-[#E500FF] to-[#990000] to-[#E500FF]">
+              <CardBody className="justify-center items-center pb-0">
+                <CircularProgress
+                  classNames={{
+                    svg: "w-36 h-36 drop-shadow-md",
+                    indicator: "stroke-white",
+                    track: "stroke-white/10",
+                    value: "text-3xl font-semibold text-white",
+                  }}
+                  value={30}
+                  strokeWidth={4}
+                  showValueLabel={true}
+                />
+              </CardBody>
+              <CardFooter className="justify-center items-center pt-0">
+                <Chip
+                  classNames={{
+                    base: "border-1 border-white/30",
+                    content: "text-white/90 text-small font-semibold",
+                  }}
+                  variant="bordered"
+                >
+                  2800 Clients Approached
+                </Chip>
+              </CardFooter>
+            </Card>
           </div>
-        </CardHeader>
-      </Card>
-      <Card className="py-4 mt-4 ">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start lg:m-0 md-m-0 m-auto">
-          <div className="flex justify-between items-center w-full">
-            <div>
-            <h4 className="font-bold text-large mb-2 w-[12rem]">Services</h4>
-              <p className="text-md uppercase mb-2 bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger bg-clip-text text-transparent font-bold">28</p>
-              {/* <small className="text-default-500">12 Sub-Services</small>
-               */}
-            </div>
-            <CardBody className="overflow-visible py-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" p-1 h-9 bg-gradient-to-r from-[#eec787] to-[#af0202] to-danger border-solid rounded-md">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
-</svg>
+        </div>
+        <div className="flex flex-col w-1/5 lg:justify-between md:justify-between justify-center  items-center ">
+          <Card className="py-4">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start lg:m-0 md-m-0 m-auto">
+              <div className="flex justify-between items-center w-full">
+                <div>
+                  <div className="flex justify-between">
+                    <div className="flex gap-1">
+                      <h4 className="text-md uppercase mb-2 font-bold text-large bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger bg-clip-text text-transparent">28</h4>
+                      <h4 className="font-bold text-large mb-2 w-[12rem]">Orders</h4>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" text-black p-1 h-9 bg-gradient-to-r from-[#fbdfb3] to-[#5bffde] to-danger border-solid rounded-md">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                    </svg>
+                  </div>
+                  {/* <small className="text-default-500">12  Clients Approached</small> */}
+
+                </div>
 
 
-            </CardBody>
-                  
-          </div>
-        </CardHeader>
-      </Card>
-      <Card className="py-4 mt-4 ">
-        <CardHeader className="pb-0 pt-2 px-4 flex-col items-start lg:m-0 md-m-0 m-auto">
-          <div className="flex justify-between items-center w-full">
-            <div>
-            <h4 className="font-bold text-large mb-2 w-[12rem]">Categories</h4>
-              <p className="text-md uppercase mb-2 bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger bg-clip-text text-transparent font-bold">28</p>
-              {/* <small className="text-default-500">12 Sub-Categories</small> */}
-              
-            </div>
-            <CardBody className="overflow-visible py-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" p-1 h-9 bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger border-solid rounded-md">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
-</svg>
+              </div>
+            </CardHeader>
+          </Card>
+          <Card className="py-4 mt-4 ">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start lg:m-0 md-m-0 m-auto">
+              <div className="flex justify-between items-center w-full">
+                <div>
+                  <div className="flex justify-between">
+                    <div className="flex gap-1">
+                      <h4 className="text-md uppercase mb-2 font-bold text-large bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger bg-clip-text text-transparent">28</h4>
+                      <h4 className="font-bold text-large mb-2 w-[12rem]">Orders</h4>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" p-1 h-9 bg-gradient-to-r from-[#eec787] to-[#af0202] to-danger border-solid rounded-md">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 21h16.5M4.5 3h15M5.25 3v18m13.5-18v18M9 6.75h1.5m-1.5 3h1.5m-1.5 3h1.5m3-6H15m-1.5 3H15m-1.5 3H15M9 21v-3.375c0-.621.504-1.125 1.125-1.125h3.75c.621 0 1.125.504 1.125 1.125V21" />
+                    </svg>
+                  </div>
+                  {/* <small className="text-default-500">12  Clients Approached</small> */}
 
-            </CardBody>
-                  
-          </div>
-        </CardHeader>
-      </Card>
-    </div>
+                </div>
+
+
+              </div>
+            </CardHeader>
+          </Card>
+          <Card className="py-4 mt-4 ">
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-start lg:m-0 md-m-0 m-auto">
+              <div className="flex justify-between items-center w-full">
+                <div>
+                  <div className="flex justify-between">
+                    <div className="flex gap-1">
+                      <h4 className="text-md uppercase mb-2 font-bold text-large bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger bg-clip-text text-transparent">28</h4>
+                      <h4 className="font-bold text-large mb-2 w-[12rem]">Orders</h4>
+                    </div>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=" p-1 h-9 bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger border-solid rounded-md">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25" />
+                    </svg>
+                  </div>
+                  {/* <small className="text-default-500">12  Clients Approached</small> */}
+
+                </div>
+
+
+              </div>
+            </CardHeader>
+
+          </Card>
+        </div>
+      </div>
     </>
   );
 }
--- app/AdminPanel/Components/circularProgress/page.jsx
+++ app/AdminPanel/Components/circularProgress/page.jsx
@@ -1,148 +1,95 @@
 "use client"
 import React from "react";
-import {CircularProgress, Card, CardBody, CardFooter, Chip} from "@nextui-org/react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Tooltip, getKeyValue} from "@nextui-org/react";
-import {EditIcon} from "./EditIcon";
-import {DeleteIcon} from "./DeleteIcon";
-import {EyeIcon} from "./EyeIcon";
-import {columns, users} from "./data";
+  import { CircularProgress, Card, CardBody, CardFooter, Chip } from "@nextui-org/react";
+import { Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, User, Tooltip, getKeyValue } from "@nextui-org/react";
+import { EditIcon } from "./EditIcon";
+import { DeleteIcon } from "./DeleteIcon";
+import { EyeIcon } from "./EyeIcon";
+import { columns, users } from "./data";
 const statusColorMap = {
-    active: "success",
-    paused: "danger",
-    vacation: "warning",
-  };
-  
+  active: "success",
+  paused: "danger",
+  vacation: "warning",
+};
+
 
 export default function CircularData() {
-    const renderCell = React.useCallback((user, columnKey) => {
-        const cellValue = user[columnKey];
-    
-        switch (columnKey) {
-          case "name":
-            return (
-              <User
-                avatarProps={{radius: "lg", src: user.avatar}}
-                description={user.email}
-                name={cellValue}
-              >
-                {user.email}
-              </User>
-            );
-          case "role":
-            return (
-              <div className="flex flex-col">
-                <p className="text-bold text-sm capitalize">{cellValue}</p>
-                <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
-              </div>
-            );
-          case "status":
-            return (
-              <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
-                {cellValue}
-              </Chip>
-            );
-          case "actions":
-            return (
-              <div className="relative flex items-center gap-2">
-                <Tooltip content="Details">
-                  <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                    <EyeIcon />
-                  </span>
-                </Tooltip>
-                <Tooltip content="Edit user">
-                  <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
-                    <EditIcon />
-                  </span>
-                </Tooltip>
-                <Tooltip color="danger" content="Delete user">
-                  <span className="text-lg text-danger cursor-pointer active:opacity-50">
-                    <DeleteIcon />
-                  </span>
-                </Tooltip>
-              </div>
-            );
-          default:
-            return cellValue;
-        }
-      }, []);
+  const renderCell = React.useCallback((user, columnKey) => {
+    const cellValue = user[columnKey];
+
+    switch (columnKey) {
+      case "name":
+        return (
+          <User
+            avatarProps={{ radius: "lg", src: user.avatar }}
+            description={user.email}
+            name={cellValue}
+          >
+            {user.email}
+          </User>
+        );
+      case "role":
+        return (
+          <div className="flex flex-col">
+            <p className="text-bold text-sm capitalize">{cellValue}</p>
+            <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+          </div>
+        );
+      case "status":
+        return (
+          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
+            {cellValue}
+          </Chip>
+        );
+      case "actions":
+        return (
+          <div className="relative flex items-center gap-2">
+            <Tooltip content="Details">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EyeIcon />
+              </span>
+            </Tooltip>
+            <Tooltip content="Edit user">
+              <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
+                <EditIcon />
+              </span>
+            </Tooltip>
+            <Tooltip color="danger" content="Delete user">
+              <span className="text-lg text-danger cursor-pointer active:opacity-50">
+                <DeleteIcon />
+              </span>
+            </Tooltip>
+          </div>
+        );
+      default:
+        return cellValue;
+    }
+  }, []);
   return (
     <>
-     <h1 className="font-bold bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger bg-clip-text text-transparent text-4xl px-8 mt-6 flex justify-center items-center">Client Progress</h1>
-    
-<div className="flex p-12 flex-wrap lg:justify-normal md:justify-center justify-center lg:gap-24 ">
+      {/* <h1 className="font-bold bg-gradient-to-r from-[#F5A524] to-[#FF705B] to-danger bg-clip-text text-transparent text-4xl px-8 mt-6 flex justify-center items-center">Client Progress</h1> */}
 
-  <Table aria-label="Example table with custom cells" className="lg:w-3/5 md:w-full w-full">
-      <TableHeader columns={columns}>
-        {(column) => (
-          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
-            {column.name}
-          </TableColumn>
-        )}
-      </TableHeader>
-      <TableBody items={users}>
-        {(item) => (
-          <TableRow key={item.id}>
-            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+      <div className="flex flex-wrap lg:justify-normal md:justify-center justify-center lg:gap-24 lg:p-12 md:p-10 p-4">
 
-    <Card className="lg:w-[31%] md:w-full w-full gap-4 bg-black lg:mt-0 md:mt-5 mt-5" >
-      <Card className=" lg:mt-0 md:mt-0 mt-1  h-[12rem] border-none  bg-gradient-to-br from-[#9C27B0] to-[#E500FF]"> 
-      <CardBody className="justify-center items-center pb-0">
-        <CircularProgress
-          classNames={{
-            svg: "w-36 h-36 drop-shadow-md",
-            indicator: "stroke-white",
-            track: "stroke-white/10",
-            value: "text-3xl font-semibold text-white",
-          }}
-          value={70}
-          strokeWidth={4}
-          showValueLabel={true}
-        />
-      </CardBody>
-      <CardFooter className="justify-center items-center pt-0">
-        <Chip
-          classNames={{
-            base: "border-1 border-white/30",
-            content: "text-white/90 text-small font-semibold",
-          }}
-          variant="bordered"
-        >
-          2800 Clients Approached   
-        </Chip>
-      </CardFooter>
-      </Card>
-      <Card className=" lg:mt-0 md:mt-0  h-[12rem] border-none  bg-gradient-to-br from-[#ffcc00] to-[#E500FF] to-[#990000] to-[#E500FF]"> 
-      <CardBody className="justify-center items-center pb-0">
-        <CircularProgress
-          classNames={{
-            svg: "w-36 h-36 drop-shadow-md",
-            indicator: "stroke-white",
-            track: "stroke-white/10",
-            value: "text-3xl font-semibold text-white",
-          }}
-          value={30}
-          strokeWidth={4}
-          showValueLabel={true}
-        />
-      </CardBody>
-      <CardFooter className="justify-center items-center pt-0">
-        <Chip
-          classNames={{
-            base: "border-1 border-white/30",
-            content: "text-white/90 text-small font-semibold",
-          }}
-          variant="bordered"
-        >
-          2800 Clients Approached   
-        </Chip>
-      </CardFooter>
-      </Card>
-    </Card>
-    </div>
+        <Table aria-label="Example table with custom cells" className="lg:w-full md:w-full w-full">
+          <TableHeader columns={columns}>
+            {(column) => (
+              <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
+                {column.name}
+              </TableColumn>
+            )}
+          </TableHeader>
+          <TableBody items={users}>
+            {(item) => (
+              <TableRow key={item.id}>
+                {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+
+        
+      </div>
     </>
   );
 }
