a77fc51d37faea508aa8b6c18536b5f98811d358
deepanshu-dhalla <dhalladeepanshu7@gmail.com>
2024-02-14 14:38:52 +0530

    latest changes

--- app/AdminPanel/page.jsx
+++ app/AdminPanel/page.jsx
@@ -4,7 +4,6 @@ import NavbarAdmin from './Components/navbar/page'
 import DataCard from './Components/DataCards/page'
 import CircularData from './Components/circularProgress/page'
 import { FooterWithSocialLinks } from '../GenresSearch/GenresComponents/GenersFooter/page'
-import { EcommerceCard } from './Components/Disclaimer/page'
 
 const page = () => {
   return (
