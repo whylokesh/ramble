d3b821d498c42c520ce12adeb02759e5881e4a64
Lokesh <jhalokesh187@gmail.com>
2024-04-03 15:17:57 +0530

    database credentials added

--- app/api/states/route.js
+++ app/api/states/route.js
@@ -2,10 +2,14 @@ import { NextResponse } from "next/server";
 import mysql from 'mysql2/promise';
 
 const db = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DATABASE
+    host: "ramble-database-prod.c7wo6qem83cz.ap-south-1.rds.amazonaws.com",
+    user: "admin",
+    database: "ramble_database",
+    password: "RambleShivam_database70145"
 });
 
 export async function GET() {
