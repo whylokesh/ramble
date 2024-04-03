3ff8c3311f14a82d3fe505755576376ed5079221
Lokesh <jhalokesh187@gmail.com>
2024-04-03 15:03:56 +0530

    get states api added

---
+++ app/api/states/route.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import mysql from 'mysql2/promise';
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+export async function GET() {
+    try {
+        const [rows] = await db.query('SELECT * FROM states');
+        return NextResponse.json(rows);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: 'Database error' }, { status: 500 });
+    }
+}
